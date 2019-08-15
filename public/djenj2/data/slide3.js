38057905bfecd2273dc9acf95a070d80a0f6d8f6
Dan Kennedy <dannykennedy@email.com>
2019-08-15 20:19:44 +1000

    djenj with ispring

---
+++ public/djenj2/data/slide3.js
@@ -0,0 +1,3 @@
+(function(){var loadHandler=window['sl_{D739F052-2A40-47D3-96E6-274653429A4D}'];loadHandler&&loadHandler(2, '<div id="spr0_f7719f6"><div id="spr1_f7719f6" class="kern slide"><img id="img1_f7719f6" src="data/img1.jpg" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_f7719f6" class="kern slide"><div id="spr3_f7719f6" style="left:130.56px;top:-0.151px;"><img id="img0_f7719f6" src="data/img4.jpg" width="1018.5" height="720" alt="A screenshot of a cell phone\
+\
+Description automatically generated" style="left:0.062px;top:0.057px;"/></div></div></div>');})();
